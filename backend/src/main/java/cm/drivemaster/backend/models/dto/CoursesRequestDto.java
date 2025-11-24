478eabe508345d4f9cdc9062e524e78157bd7dae
Devmodjo <yvankamsu88@gmail.com>
2025-11-24 09:30:51 +0100

    mise a point des contraintes de validation sur les dto

--- backend/src/main/java/cm/drivemaster/backend/models/dto/CoursesRequestDto.java
+++ backend/src/main/java/cm/drivemaster/backend/models/dto/CoursesRequestDto.java
@@ -1,8 +1,18 @@
 package cm.drivemaster.backend.models.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public record CoursesRequestDto(
+
+        @Positive
+        @NotBlank(message = "l'identifiant de l'école est obligatoire")
         Long drivingSchoolId,
+
+        @NotBlank(message = "le titre du cours est obligatoire")
         String title,
+
+        @NotBlank(message = "le contenu du cours est obligatoire")
         String content
 ) {
 }
