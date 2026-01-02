dfeff4b00b9f5385bae77b77754ca6dfa806cf41
Devmodjo <yvankamsu88@gmail.com>
2026-01-02 20:09:44 +0100

    mise en place de la logique d'authentification d'un admin

--- backend/src/main/java/cm/drivemaster/backend/repositories/PlatformAdminRepository.java
+++ backend/src/main/java/cm/drivemaster/backend/repositories/PlatformAdminRepository.java
@@ -3,14 +3,19 @@ package cm.drivemaster.backend.repositories;
 
 import cm.drivemaster.backend.beans.PlatformAdmin;
 import cm.drivemaster.backend.enums.AdminRole;
+import cm.drivemaster.backend.enums.AdminStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PlatformAdminRepository extends JpaRepository<PlatformAdmin, Long> {
 
     boolean existsByRole(AdminRole role);
+
     Optional<PlatformAdmin> findByEmail(String email);
+
+    List<PlatformAdmin> findByStatus(AdminStatus status);
 }
